79c78ea8f9b53c5ca86477b1869e602deb037c44
Taiki ONO <taiks.4559@gmail.com>
2014-02-10 23:07:47 +0900

    Add one by one example for shortLinesOnly

--- chapter-9/shortLinesOnly.hs
+++ chapter-9/shortLinesOnly.hs
@@ -11,3 +11,17 @@ shortLinesOnly = unlines . (filter $ isShort 10) . lines
 
 isShort :: Int -> String -> Bool
 isShort n line = length line < n
+
+
+
+
+-- ===============================
+shortLinesOnly' :: String -> String
+-- content is like:
+--      abdfg
+--      1234567890123
+--      AAAAABBB
+shortLinesOnly' content = let contentLines = lines content
+                                            -- filter :: (a -> Bool) -> [a] -> [a]
+                              onlyShortLines = filter (\line -> length line < 10) contentLines
+                           in unlines onlyShortLines
